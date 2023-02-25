2631c15ae40dc4ac28b7bbfe1f9d7c98eeabf372
Aman Chaudhry <30852046+amansabarwal@users.noreply.github.com>
2023-02-25 21:59:43 +0530

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,61 @@
+#get acess google drive data into google colab
+from google.colab import drive
+drive.mount('/content/drive')
+
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+# Initialising the CNN
+CNN_Classifier=Sequential();
+
+# Step 1 - Convolution
+CNN_Classifier.add(Conv2D(32,(3,3),input_shape=(64,64,3),activation='relu'))
+
+# Step 2 - Pooling
+CNN_Classifier.add(MaxPooling2D(pool_size=(2,2)))
+
+# Step 1 - Convolution
+CNN_Classifier.add(Conv2D(16,(3,3),activation='relu'))
+
+# Step 2 - Pooling
+CNN_Classifier.add(MaxPooling2D(pool_size=(2,2)))
+
+
+# Step 3 - Flattening
+CNN_Classifier.add(Flatten())
+
+
+# Step 4 - Full connection
+CNN_Classifier.add(Dense(units=128, activation='relu'))
+CNN_Classifier.add(Dense(units=1, activation='sigmoid'))
+
+# Compiling the CNN
+CNN_Classifier.compile(optimizer ='adam',loss='binary_crossentropy',metrics=['accuracy'])
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('/content/drive/MyDrive/dataset/training_set',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+test_set = test_datagen.flow_from_directory('/content/drive/MyDrive/dataset/test_set',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+CNN_Classifier.fit_generator(training_set,validation_data = test_set,
+                         epochs = 3, steps_per_epoch = 10,
+                         validation_steps = 200)
